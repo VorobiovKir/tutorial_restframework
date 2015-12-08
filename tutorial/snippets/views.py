15d92eb9c3dfadabecb22d4ebc8324b745f0abde
VorobiovKir <kvorobiov89@gmail.com>
2015-12-08 17:19:12 +0200

    add user

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import permissions
 
 from .models import Snippet
 from .serializers import SnippetSerializer, UserSerializer
@@ -9,11 +10,16 @@ from django.contrib.auth.models import User
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permissions_class = (permissions.IsAuthenticatedOrReadOnly)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permissions_class = (permissions.IsAuthenticatedOrReadOnly)
 
 
 class UserList(generics.ListAPIView):

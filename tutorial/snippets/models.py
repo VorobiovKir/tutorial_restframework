e71537baaf9f13e5848499b83392e3d9fe93980b
VorobiovKir <kvorobiov89@gmail.com>
2015-12-08 17:09:52 +0200

    add user

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -2,8 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from pygments.lexers import get_all_lexers
+from pygments.lexers import get_all_lexers, get_lexer_by_name
 from pygments.styles import get_all_styles
+from pygments.formatters.html import HtmlFormatter
+from pygments import highlight
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
@@ -17,6 +19,17 @@ class Snippet(models.Model):
     linenos = models.BooleanField(default=False)
     language = models.CharField(max_length=100, choices=LANGUAGE_CHOICES, default='python')
     style = models.CharField(max_length=100, default='friendly', choices=STYLE_CHOICES)
+    owner = models.ForeignKey('auth.User', related_name='snippets')
+    highlighted = models.TextField()
 
     class Meta:
         ordering = ('created', )
+
+    def save(self, *args, **kwargs):
+        lexer = get_lexer_by_name(self.language)
+        linenos = self.linenos and 'table' or False
+        options = self.title and {'title': self.title} or {}
+        formatter = HtmlFormatter(style=self.style, linenos=linenos,
+                                  full=True, **options)
+        self.highlighted = highlight(self.code, lexer, formatter)
+        super(Snippet, self).save(*args, **kwargs)
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,7 +1,9 @@
 from rest_framework import generics
 
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import SnippetSerializer, UserSerializer
+
+from django.contrib.auth.models import User
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -12,3 +14,13 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
